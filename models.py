f17e1e14aba4d24ade6de97fef3c9da340652a4f
CHETAN RATHOD <141659659+RathodChetan1122@users.noreply.github.com>
2026-04-08 21:56:14 +0530

    Fix formatting in models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 db = SQLAlchemy() 
-
+  
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True) 
